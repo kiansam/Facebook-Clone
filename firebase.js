5e930edae4ca39a9b4ae20c01fdadc61914957c5
Kian Sam <itskiansam@gmail.com>
2021-07-15 10:03:08 +0300

    firebase.js

---
+++ firebase.js
@@ -0,0 +1,20 @@
+import firebase from "firebase";
+import "firebase/storage";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDA4uRyR4DyC7b_eE5nMqHgkcaa8mScLz0",
+  authDomain: "facebook-cl1.firebaseapp.com",
+  projectId: "facebook-cl1",
+  storageBucket: "facebook-cl1.appspot.com",
+  messagingSenderId: "13190349013",
+  appId: "1:13190349013:web:ee1da2fa9732edbf8aa25c",
+};
+
+const app = !firebase.app.length
+  ? firebase.initializeApp(firebaseConfig)
+  : firebase.app();
+
+const db = app.firestore();
+const storage = firebase.storage();
+
+export { db, storage };
