33838db6797b9d2389e434825178ed51f572ad38
Kian Sam <itskiansam@gmail.com>
2021-07-15 17:57:06 +0300

    Upload

--- components/InputBox.js
+++ components/InputBox.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 import { EmojiHappyIcon } from "@heroicons/react/outline";
 import { CameraIcon, VideoCameraIcon } from "@heroicons/react/solid";
 import { useRef, useState } from "react";
-import { db } from "../firebase";
+import { db, storage } from "../firebase";
 import firebase from "firebase";
 
 function InputBox() {
@@ -17,18 +17,48 @@ function InputBox() {
 
     if (!inputRef.current.value) return;
 
-    db.collection("posts").add({
-      message: inputRef.current.value,
-      name: session.user.name,
-      email: session.user.email,
-      image: session.user.image,
-      timestamp: firebase.firestore.FieldValue.serverTimestamp(),
-    });
+    db.collection("posts")
+      .add({
+        message: inputRef.current.value,
+        name: session.user.name,
+        email: session.user.email,
+        image: session.user.image,
+        timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+      })
+      .then((doc) => {
+        if (imageToPost) {
+          const uploadTask = storage
+            .ref(`posts/${doc.id}`)
+            .putString(imageToPost, "data_url");
+
+          removeImage();
+
+          uploadTask.on(
+            "state_change",
+            null,
+            (error) => console.error(error),
+            () => {}
+          );
+        }
+      });
 
     inputRef.current.value = "";
   };
 
-  const addImageToPost = (e) => {};
+  const addImageToPost = (e) => {
+    const reader = new FileReader();
+    if (e.target.files[0]) {
+      reader.readAsDataURL(e.target.files[0]);
+    }
+
+    reader.onload = (readerEvent) => {
+      setImageToPost(readerEvent.target.result);
+    };
+  };
+
+  const removeImage = () => {
+    setImageToPost(null);
+  };
 
   return (
     <div className="bg-white p-2 rounded-xl shadow-md text-gray-500 font-medium mt-6">
@@ -52,7 +82,21 @@ function InputBox() {
             Submit
           </button>
         </form>
+        {imageToPost && (
+          <div
+            onClick={removeImage}
+            className="flex flex-col filter hover:brightness-75 transition duration-150 transform hover:scale-105 cursor-pointer"
+          >
+            <img
+              className="h-10 object-contain"
+              src={imageToPost}
+              alt="imageToPost"
+            />
+            <p className="text-xs text-red-500 text-center">Remove</p>
+          </div>
+        )}
       </div>
+
       <div className="flex justify-evenly p-3 border-t">
         <div className="inputIcon">
           <VideoCameraIcon className="h-7 text-red-500" />
